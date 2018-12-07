a2e33835966ba6a0bb27cb729a0af29391dbdcf0
Andrea Cifola <cifola.andrea@gmail.com>
2018-12-07 09:45:40 +0100

    Refactor Main class

--- src/main/java/com/example/demo/PaymentServiceMain.java
+++ src/main/java/com/example/demo/PaymentServiceMain.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentServiceApplication {
+public class PaymentServiceMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentServiceApplication.class, args);
+		SpringApplication.run(PaymentServiceMain.class, args);
 	}
 }
